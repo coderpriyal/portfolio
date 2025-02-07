47cd5ff8af2681a3ead4debe4ff54f4812933071
coderpriyal <priyal.keharia@somaiya.edu>
2025-02-07 22:20:20 +0530

    Updated header component

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,16 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+
+
 @NgModule({
   declarations: [
+    HeaderComponent,
+
   ],
   imports: [
     CommonModule,
     FontAwesomeModule,
+    TranslateModule, 
+    TranslateModule.forRoot(),
+    FormsModule,
+    BrowserAnimationsModule
+
     
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
 export const routes: Routes = [
     { path: '', component: AppComponent }, 
+    { path: 'header', component:HeaderComponent}
+    
 ];
